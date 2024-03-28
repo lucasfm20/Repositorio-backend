5d179b7287bbe043dffc87ade76ef0b7cf659874
lucasfm20 <138518985+lucasfm20@users.noreply.github.com>
2024-03-27 21:28:37 -0300

    Add files via upload

---
+++ exercicio3/src/main/java/org/example/Ator.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Ator implements Indicavel{
+   private String nome;
+    private String nacionalidade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+}
